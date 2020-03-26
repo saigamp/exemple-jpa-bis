3bfdac826f7d55e205347de038ef08aa75e3392c
Perso <Perso@maison.home>
2020-03-26 14:55:08 +0100

    base H2 + book

--- src/main/java/fr/saigamp/exemplejpabis/Application.java
+++ src/main/java/fr/saigamp/exemplejpabis/Application.java
@@ -1,33 +1,40 @@
 package fr.saigamp.exemplejpabis;
 
-import java.util.Arrays;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+
+import fr.saigamp.exemplejpabis.repository.BookRepository;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    private BookRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-//	@Bean
-//	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-//		return args -> {
-//
-//			System.out.println("Let's inspect the beans provided by Spring Boot:");
-//
-//			String[] beanNames = ctx.getBeanDefinitionNames();
-//			Arrays.sort(beanNames);
-//			for (String beanName : beanNames) {
-//				System.out.println(beanName);
-//			}
-//
-//		};
-//	}
+    @Override
+    public void run(String... args) {
+
+        log.info("StartApplication...");
+
+        System.out.println("\nfindAll()");
+        repository.findAll().forEach(x -> System.out.println(x.getName()));
+
+        System.out.println("\nfindById(1L)");
+        repository.findById(1l).ifPresent(x -> System.out.println(x.getName()));
+
+        System.out.println("\nfindByName('Node')");
+        repository.findByName("Node").forEach(x -> System.out.println(x.getName()));
+
+    }
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO book VALUES(1, 'Java');
+INSERT INTO book VALUES(2, 'Node');
+INSERT INTO book VALUES(3, 'Python');
